7001a3a7307ca63df7fd954a381f8b466e9c1609
Soengyong Kim <soekim@c6r9s4.42seoul.kr>
2021-03-04 04:46:08 +0900

    doing ch4, implemented basic vetor calculating

---
+++ oneweekend/ch4.c
@@ -0,0 +1,82 @@
+
+#include <stdio.h>
+#include <math.h>
+#include <mlx.h>
+#include "./myvector.h"
+
+// basic mlx functions
+typedef struct	s_vars 
+{
+	void	*mlx;
+	void	*win;
+}				t_vars;
+
+typedef struct	s_data{
+	void *img;
+	char *addr;
+	int bpp, linelen, endian;
+}				t_data;
+
+void			pixput(t_data *img, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = img->addr + (y * img->linelen + x * (img->bpp / 8));
+	*(unsigned int *)dst = color;
+}
+
+//ray tracing functions
+typedef struct	s_ray
+{
+	t_vec		orig;
+	t_vec		curr;
+	t_vec		dir;
+}				t_ray;
+
+void		initray(t_ray *ray, t_vec orig, t_vec dir)
+{
+	ray->orig = orig;
+	ray->curr = orig;
+	ray->dir = normalize(dir);
+}
+
+void		cast(t_ray *ray, double t)
+{
+	r->curr.x += (r->dir.x * t);
+	r->curr.y += (r->dir.y * t);
+	r->curr.z += (r->dir.z * t);
+}
+
+typedef struct	s_plane
+{
+	double width, height;
+}				t_plane;
+
+int main()
+{
+	t_vars	ex;
+	t_data	img;
+ 
+	//	ch 4
+	//	image
+	const double scrratio = 16.0/9.0;
+	t_plane	img;
+	img.width = 400;
+	img.height = img.width / scrratio;
+
+	//camera
+	t_plane	view;
+	view.height = 2.0;
+	view.width = scrraitio * view.height;
+	double	focallen = 1.0;
+
+	vec origin, horizontal, vertical, lowerleft;
+	initvec(&origin, 0, 0, 0);
+	initvec(&horizontal, view.width, 0, 0);
+	initvec(&vertical, 0, view.height, 0);
+	lowerleft = minus(origin,add(multi(horizontal, 0.5)
+
+	mlx_put_image_to_window(ex.mlx, ex.win, img.img, 25, 25);
+	mlx_loop(ex.mlx);
+	
+}
---
+++ oneweekend/myvector.h
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <math.h>
+
+typedef struct	s_vector
+{
+	double x,y,z;
+}				t_vec;
+
+void			initvec(vec *v, double x, double y, double z)
+{
+	v->x = x;
+	v->y = y;
+	v->z = z;
+}
+
+t_vec		multi(const t_vec *v, double n)
+{
+	t_vec result;
+	result.x = v->x * n;
+	result.y = v->y * n;
+	result.z = v->z * n;
+	return (result);
+}
+
+t_vec		normalize(const t_vec *v)
+{
+	t_vec	result;
+	double	norm;
+
+	norm = sqrt(pow(v->x, 2) + pow(v->y, 2) + pow(v->z, 2));
+	result.x /= norm;
+	result.y /= norm;
+	result.z /= norm;
+	return (result);
+}
+
+t_vec		add(const t_vec *v1, const t_vec *v2)
+{
+	t_vec result;
+
+	result.x = v1->x + v2->x;
+	result.y = v1->y + v2->y;
+	result.z = v1->z + v2->z;
+	return (result);
+}
+
+t_vec		minus(const t_vec *v1, const t_vec *v2)
+{
+	t_vec result;
+
+	result.x = v1->x - v2->x;
+	result.y = v1->y - v2->y;
+	result.z = v1->z - v2->z;
+	return (result);
+}
+
+double	innerprod(const t_vec *v1, const t_vec *v2)
+{
+	return (v1->x * v2->x + v1->y * v2->y + v1->z * v2->z);
+}
+
+
