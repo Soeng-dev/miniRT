87723cf4c38366d55ca8da98799423edadb019be
Soengyong Kim <soekim@c4r10s4.42seoul.kr>
2021-03-04 18:41:10 +0900

    ch4 done

--- oneweekend/myvector.h
+++ oneweekend/myvector.h
@@ -6,57 +6,66 @@ typedef struct	s_vector
 	double x,y,z;
 }				t_vec;
 
-void			initvec(vec *v, double x, double y, double z)
+void			initvec(t_vec *v, double x, double y, double z)
 {
 	v->x = x;
 	v->y = y;
 	v->z = z;
 }
 
-t_vec		multi(const t_vec *v, double n)
+t_vec		multivec(t_vec v, double n)
 {
 	t_vec result;
-	result.x = v->x * n;
-	result.y = v->y * n;
-	result.z = v->z * n;
+	result.x = v.x * n;
+	result.y = v.y * n;
+	result.z = v.z * n;
 	return (result);
 }
 
-t_vec		normalize(const t_vec *v)
+t_vec		divvec(t_vec v, double n)
+{
+	t_vec result;
+	result.x = v.x / n;
+	result.y = v.y / n;
+	result.z = v.z / n;
+	return (result);
+}
+
+t_vec		normalize(t_vec v)
 {
 	t_vec	result;
 	double	norm;
 
-	norm = sqrt(pow(v->x, 2) + pow(v->y, 2) + pow(v->z, 2));
+	norm = sqrt(pow(v.x, 2) + pow(v.y, 2) + pow(v.z, 2));
 	result.x /= norm;
 	result.y /= norm;
 	result.z /= norm;
 	return (result);
 }
 
-t_vec		add(const t_vec *v1, const t_vec *v2)
+t_vec		addvec(t_vec v1, t_vec v2)
 {
 	t_vec result;
 
-	result.x = v1->x + v2->x;
-	result.y = v1->y + v2->y;
-	result.z = v1->z + v2->z;
+	result.x = v1.x + v2.x;
+	result.y = v1.y + v2.y;
+	result.z = v1.z + v2.z;
 	return (result);
 }
 
-t_vec		minus(const t_vec *v1, const t_vec *v2)
+t_vec		minusvec(t_vec v1, t_vec v2)
 {
 	t_vec result;
 
-	result.x = v1->x - v2->x;
-	result.y = v1->y - v2->y;
-	result.z = v1->z - v2->z;
+	result.x = v1.x - v2.x;
+	result.y = v1.y - v2.y;
+	result.z = v1.z - v2.z;
 	return (result);
 }
 
-double	innerprod(const t_vec *v1, const t_vec *v2)
+double	innerprod(t_vec v1, t_vec v2)
 {
-	return (v1->x * v2->x + v1->y * v2->y + v1->z * v2->z);
+	return (v1.x * v2.x + v1.y * v2.y + v1.z * v2.z);
 }
 
 
