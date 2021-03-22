d486d2e0db4c099b35fd900202d3f65e7e03d566
yong-dev <tjddyd1565@naver.com>
2021-03-23 02:05:43 +0900

    libft added, some unnecessary files deleted

--- oneweekend/comp/diffused.sh
+++
@@ -1,1 +0,0 @@
-gcc -lmlx -framework OpenGL -framework AppKit $1 miniRT.h diffused_main.c libft/*.? utils/*.? ray_tracing/*.?
--- oneweekend/comp/linux-diffused.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-gcc diffused_main.c miniRT.h libft/*.? utils/*.? ray_tracing/*.? $1 -lbsd -lmlx -lXext -lX11 -lm
