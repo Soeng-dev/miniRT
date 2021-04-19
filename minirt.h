c3b11415ede7d538b6c4e9f7e32acaecb3f72f9c
Soengyong Kim <soekim@c4r10s4.42seoul.kr>
2021-04-19 18:02:47 +0900

    for change name of minirt.h

---
+++ minirt.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minirt.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: soekim <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/09 16:57:44 by soekim            #+#    #+#             */
+/*   Updated: 2020/11/21 16:14:32 by soekim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINIRT_H
+# define MINIRT_H
+# define _USE_MATH_DEFINES
+# define STDIN 0
+
+# include <mlx.h>
+# include <math.h>
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <stdio.h>
+# include <stdlib.h>
+
+# include "libft/libft.h"
+# include "utils/math/myvector.h"
+# include "utils/bmp_tools.h"
+# include "utils/math/math_tools.h"
+# include "utils/mymlx_utils.h"
+# include "ray_tracing/ray_tracing.h"
+# include "get_next_line/get_next_line.h"
+# include "manage_command/manage_command.h"
+
+int		save_scene(char *path, t_info *info);
+
+#endif
